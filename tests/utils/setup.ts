22b8854466167411f66fc8341ab2502e0f1b4959
adamcharusta <charustaadam95@gmail.com>
2025-07-17 00:43:45 +0200

    Refactor test setup: update imports and extend matchers for Vitest

--- tests/utils/setup.ts
+++ tests/utils/setup.ts
@@ -1,2 +1,3 @@
-import '@testing-library/jest-dom/jest-globals';
-import '@testing-library/jest-dom';
+import matchers from '@testing-library/jest-dom/matchers';
+import { expect } from 'vitest';
+expect.extend(matchers);
